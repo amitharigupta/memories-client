ad905c6a51d0100a091c625c7ec1ea59be1a8fe2
amitharigupta <amitgt9967@gmail.com>
2022-01-30 11:42:52 +0530

    add, update is working in frontend

---
+++ src/reducers/posts.js
@@ -0,0 +1,12 @@
+export default (posts = [], action) => {
+  switch (action.type) {
+    case "FETCH_ALL":
+      return action.payload.data;
+    case "CREATE":
+      return [...posts, action.payload];
+    case "UPDATE":
+      return posts.map((post) => post._id === action.payload.data._id ? action.payload.data : post);
+    default:
+      return posts;
+  }
+};
